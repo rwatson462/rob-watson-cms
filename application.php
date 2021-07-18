f89a0820e3ce80ed129ec0282edbeb4264b04290
= <=>
2021-07-18 20:39:51 +0100

    move request-related functions into own file

--- application.php
+++ application.php
@@ -7,6 +7,7 @@
 
 // load config file from outside the public directory
 include __DIR__ . '/config.php';
+include __DIR__ . '/include/request.php';
 
 // include database class and fetch an instance
 include __DIR__ . '/vendor/SourcePot/Database/Database.php';
@@ -19,31 +20,3 @@ Database::pool(
    pool_name: 'cms'
 );
 
-
-/**
- * Determine if the current request has been made with the 'POST' method.
- * Uses $_SERVER['REQUEST_METHOD'] to determine the request method
- * @return bool
- */
-function isPostRequest(): bool
-{
-   return $_SERVER['REQUEST_METHOD'] === 'POST';
-}
-
-/**
- * Determine if the requested query parameter exists.
- * Uses $_REQUEST to check for the parameter
- * @param string $param the parameter to check for
- * @return bool
- */
-function hasQueryParam( string $param ): bool
-{
-   return array_key_exists( $param, $_REQUEST );
-}
-
-function queryParam( string $param ): string
-{
-   if( hasQueryParam( $param ) ) return $_REQUEST[$param];
-   return null;
-
-}
